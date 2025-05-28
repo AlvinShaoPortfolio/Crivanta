532dd9f87e3766ddb612531630063cf4c969fb8a
Alvin Shao <Alvinshao518@gmail.com>
2025-05-27 20:28:07 -0400

    Remove firebase configs just in case

--- lib/profileCreation.dart
+++ lib/profileCreation.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-
 
 class ProfileCreationScreen extends StatelessWidget{
   const ProfileCreationScreen({super.key});
-
 
   @override
   Widget build(BuildContext context){
@@ -14,26 +11,21 @@ class ProfileCreationScreen extends StatelessWidget{
   }
 }
 
-
 class CharacterContainer extends StatefulWidget{
   const CharacterContainer({super.key});
-
 
   @override
   State<CharacterContainer> createState() => _CharacterContainer();
 }
 
-
 class _CharacterContainer extends State<CharacterContainer>{
   bool pressed = false;
   bool showSkills = false;
-
 
   void togglePressed() {
     setState(() {
       pressed = !pressed;
     });
-
 
     if (pressed) {
       Future.delayed(const Duration(milliseconds: 100), () {
@@ -51,10 +43,8 @@ class _CharacterContainer extends State<CharacterContainer>{
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-
 
     return Stack(
         children: [
@@ -71,18 +61,14 @@ class _CharacterContainer extends State<CharacterContainer>{
   }
 }
 
-
 class CharacterText extends StatelessWidget{
   final bool pressed;
 
-
   const CharacterText({super.key, required this.pressed});
-
 
   @override
   Widget build(BuildContext context){
     Alignment placement = pressed ? Alignment(0.0, -0.9) : Alignment(0.0, -0.35);
-
 
     return AnimatedContainer(
       duration: Duration(milliseconds: 250),
@@ -90,24 +76,19 @@ class CharacterText extends StatelessWidget{
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children:[
-          Text(('Your Journey Begins'),
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          Text(('Create your character and start your adventure'),
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+          Text(('Your Journey Begins'), style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          Text(('Create your character and start your adventure'), style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
         ],
       ),
     );
   }
 }
 
-
 class CharacterIcon extends StatelessWidget{// stateless because managed by the container
   final bool pressed;
   final VoidCallback onPressed;
 
-
   const CharacterIcon({super.key, required this.pressed, required this.onPressed});
-
 
   @override
   Widget build(BuildContext context){
@@ -127,79 +108,40 @@ class CharacterIcon extends StatelessWidget{// stateless because managed by the 
   }
 }
 
-
-/*
-class LinkToSkill extends StatelessWidget{
- final Offset endPoint, startPoint;
- final Color color;
-
-
- const LinkToSkill({super.key, required this.startPoint, required this.endPoint, required this.color});
-
-
- @override
- Widget build(BuildContext context){
-   final dx = endPoint.dx - startPoint.dx;
-   final dy = endPoint.dy - startPoint.dy;
-   final length = sqrt(dx * dx + dy * dy);
-   final angle = atan2(dy, dx);
-
-
-   return Positioned(
-     left: startPoint.dx,
-     top: startPoint.dy,
-     child: Transform.rotate(
-       angle: angle,
-       alignment: Alignment.topLeft,
-       child: Container(
-         width: length,
-         height: 5,
-         color: color,
-       ),
-     ),
-   );
- }
-}
-*/
-
-
 class SkillsIcon extends StatelessWidget{
   final bool showSkills;
   final String name;
   final Color myColor;
   final double xCoord, yCoord;
 
-
   const SkillsIcon({super.key, required this.name, required this.showSkills, required this.myColor, required this.xCoord, required this.yCoord});
-
 
   @override
   Widget build(BuildContext context){
     Alignment placement = showSkills ? Alignment(xCoord, yCoord) : Alignment(0.0, 0.0);
 
-
     return AnimatedOpacity(//controlling the fading in
-        opacity: showSkills ? 1.0 : 0.0,
+      opacity: showSkills ? 1.0 : 0.0,
+      duration: Duration(milliseconds: 250),
+      child: AnimatedAlign(//controlling the axis change
         duration: Duration(milliseconds: 250),
-        child: AnimatedAlign(//controlling the axis change
-            duration: Duration(milliseconds: 250),
-            alignment: placement,
-            child: Stack(
-                children: [
-                  Container( //the actual box
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Center(
-                      child: Text(name, style: const TextStyle(color: Colors.white)),
-                    ),
-                  ),
-                ]
-            )
+        alignment: placement,
+        child: Stack(
+          children: [
+            Container( //the actual box
+              width: 100,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Center(
+                child: Text(name, style: const TextStyle(color: Colors.white)),
+              ),
+            ),
+          ]
         )
+      )
     );
   }
 }
